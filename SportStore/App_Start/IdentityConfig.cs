28d7e8ac9b5ba1ab0cef66e12ee89cb7456c6549
ashafir <aviv@optimalfinance.co.il>
2014-11-06 18:29:09 +0200

    added null check and removed xml formatter

--- SportStore/App_Start/IdentityConfig.cs
+++ SportStore/App_Start/IdentityConfig.cs
@@ -15,10 +15,17 @@ namespace SportStore
             app.CreatePerOwinContext<StoreUserManager>(StoreUserManager.Create);
             app.CreatePerOwinContext<StoreRoleManager>(StoreRoleManager.Create);
 
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
-            });
+        /*  I configured ASP.NET Identity so that it will set a cookie when a request is successfully authenticated, which allows
+            subsequent requests from the same client to be authorized without needing credentials. The cookie is required for
+            round-trip applications, and the authentication test I have used in this chapter will not work without it. I will be using
+            a different approach for the SportsStore application, which is to explicitly set an HTTP header to provide proof that
+            the client has been authenticated. I explain this process in Chapter 6, but my final step in this chapter is to disable the
+            cookie, as shown in Listing 5-22.*/
+
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions
+            //{
+            //    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+            //});
         }
     }
 }
---
+++ SportStore/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private IRepository Repository { get; set; }
+
+        public ProductsController()
+        {
+            Repository = new ProductRepository();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return Repository.Products;
+        }
+
+        public Product GetProduct(int id)
+        {
+            Product result = Repository.Products.FirstOrDefault(p => p.Id == id);
+            if (result == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        public async Task PostProduct(Product product)
+        {
+            await Repository.SaveProductAsync(product);
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            await Repository.DeleteProductAsync(id);
+        }
+    }
+}
